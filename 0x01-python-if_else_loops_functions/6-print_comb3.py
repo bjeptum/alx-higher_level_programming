c45157eaedd53c41af6f88300644043a79cf8627
bjeptum <brendajeptum@gmail.com>
2024-01-17 08:27:46 +0000

    Combine digits between 0 and 9 and print out

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 10):
-    for i in range(0, 10):
-        print("{}{}".format(num, i), end=", ")
+    for i in range(num+1, 10):
+        if (num != 8):
+            print("{}{}".format(num, i), end=(", "))
+        else:
+            print("{}{}".format(num, i))
