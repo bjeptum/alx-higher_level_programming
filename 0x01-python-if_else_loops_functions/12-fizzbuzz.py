0a0baa213a4341d75531d586260ada19e9cf0bc6
bjeptum <brendajeptum@gmail.com>
2024-01-17 14:14:49 +0000

    Fizz Buzz updated

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,7 +9,7 @@ multiples of both 3 and 5 print FizzBuzz
 
 
 def fizzbuzz():
-    for num in range(1, 100):
+    for num in range(1, 101):
         if (num % 15 == 0):
             print("FizzBuzz", end=" ")
         elif (num % 3 == 0):
