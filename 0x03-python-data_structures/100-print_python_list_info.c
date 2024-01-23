63f90c08fd7aa0f5cf240f78d57f6a55e2bedc4e
bjeptum <brendajeptum@gmail.com>
2024-01-23 14:50:33 +0000

    CPython #0: Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_python_list_info - Prints info about Python lists
+ * @p: Python object - list
+ * Return: Type of elements in lis, size and memory
+ */
+void print_python_list_info(PyObject *p)
+{
+
+}
