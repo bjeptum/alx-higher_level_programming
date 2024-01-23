60979ec7697e1b2ecd5d4fb73f8d461d3e6acfc8
bjeptum <brendajeptum@gmail.com>
2024-01-23 12:57:24 +0000

    Prints a matrix of integers updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,5 +8,7 @@ def print_matrix_integer(matrix=[[]]):
     length = len(matrix)
     for i in range(length):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != len(matrix[i]) - 1:
+                print(" ", end="")
         print()
