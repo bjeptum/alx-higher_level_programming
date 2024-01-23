a5f087a4342a597fa9acde01a9bcc0e9cfafca4d
bjeptum <brendajeptum@gmail.com>
2024-01-23 14:28:02 +0000

    Retrieve an element in a list at a specified index updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,10 +6,8 @@ Retrieves an element from a list
 
 def element_at(my_list, idx):
     length = len(my_list)
-    elem = my_list[idx]
-    if idx < 0:
-        return None
-    elif idx >= length:
+    if idx < 0 or idx >= length:
         return None
     else:
+        elem = my_list[idx]
         return elem
