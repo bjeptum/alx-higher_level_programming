7e006e03365f38fdb233d7c895ce412e529f806a
bjeptum <brendajeptum@gmail.com>
2024-01-23 13:13:51 +0000

    Tuples addition uodated

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,5 +7,5 @@ Adds 2 tuples
 def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
     b = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
-    tuple_c = tuple(map(lambda i, j: i + j, a, b))
+    tuple_c = (a[0] + b[0], a[1] + b[1])
     return tuple_c
