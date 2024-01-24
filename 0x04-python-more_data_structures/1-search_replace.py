83f024996dbfc280fca78675b71123a73a319ea6
bjeptum <brendajeptum@gmail.com>
2024-01-24 17:30:56 +0000

    Search and replace an element in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,9 @@
 Replaces all occurrences
 of an element by another in a new list
 """
+
+
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda item: replace if item == search else item, my_list))
+    new_list = list(map(lambda item: replace
+                    if item == search else item, my_list))
     return new_list
