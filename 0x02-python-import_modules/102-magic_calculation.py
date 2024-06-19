0cd0f213de417144696e29e9be5fd6d7dccd2e29
bjeptum <brendajeptum@gmail.com>
2024-06-19 09:38:47 +0000

    ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,15 +4,13 @@ from magic_calculation_102 import add, sub
 
 
 def magic_calculation(a, b):
-    c = 0
-    if a > b:
-        c = sub(a, b)
-        if c == 4:
-            c = add(c, 10)
     if a < b:
-        c = add(a, b)
-        for i in range(4, 7):
-            c = add(a, i)
-    else:
+        return add(a,b)
+    elif a > b:
         c = sub(a, b)
-    return c
+        if c < 4:
+            return 0
+        else:
+            return add(c, 10)
+    else:
+        return a * b
