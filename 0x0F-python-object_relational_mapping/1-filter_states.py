d74632caf3bf422b305d97a0edfb56feb1730995
bjeptum <brendajeptum@gmail.com>
2024-06-23 19:28:32 +0000

    Lists all states starting with N in database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Lists all states from database hbtn_0e_0_usa
+Lists all states from database base hbtn_0e_0_usa
+Starting with N(upper N)
 Ordered by states.id
-Used the module MySQLdb (import MySQLdb)
+Used the module MySQLdb
 """
 
 
@@ -10,15 +11,17 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # Connecting to hbtn_0e_0_usa d
+
     db = MySQLdb.connect(host="localhost",
                          user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306)
+    # Create cursor
     curx = db.cursor()
-    curx.execute("SELECT * FROM states ORDER BY id ASC"))
+    # Execute select to query data
+    curx.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = curx.fetchall()
     for row in rows:
         print(row)
-    # Close  cursor and db
+        # Close  cursor and db
     curx.close()
     db.close()
