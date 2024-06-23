840c768cfe24fd867f9f975c64ed676f1ca29171
bjeptum <brendajeptum@gmail.com>
2024-06-23 22:08:31 +0000

    City module defined

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Lists all prints from the database
+"""
+
+from sys import argv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine, MetaData
+from sqlalchemy.engine import result
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == '__main__':
+
+    # Connect to MySQL server running on localhost
+    username, password, database = argv[1], argv[2], argv[3]
+
+    # Create engine and connect to db
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+                             {password}@localhost:3306/{database}')
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query database and print results
+    results = session.query(Cities.order_by(City.id)
+    for state in results:
+        print(f"{state.id}: {state.name}")
