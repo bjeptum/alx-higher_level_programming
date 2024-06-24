1417b239f4f8b08ee97f8f4e4cf54c969517503a
bjeptum <brendajeptum@gmail.com>
2024-06-24 20:36:57 +0000

    Square class with attribute defined

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defined Square Module
+"""
+
+
+class Square:
+    "Define square with size"
+    def __init__(self, size):
+        " Class instantiator"
+        self.__size = size
