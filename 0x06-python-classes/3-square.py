29ed4ebe019f8639e956ad91539888e1e9da6827
bjeptum <brendajeptum@gmail.com>
2024-06-24 20:57:09 +0000

    Square module with area method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defined Square Module
+"""
+
+
+class Square:
+    "Define square with size"
+    def __init__(self, size=0):
+        " Class instantiator"
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        " Calculates area of square"
+        return self.__size ** 2
