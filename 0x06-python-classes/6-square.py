e4edc05b7e0894f8058a35433ee27695cfc4cad0
bjeptum <brendajeptum@gmail.com>
2024-06-26 20:40:54 +0000

    Coordinates ofsquare included in its description

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Defined Square Module
+"""
+
+
+class Square:
+    """
+    Define square with attribute size
+    with methods to retrieve and set size
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        " Class instantiator"
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        " Getter method for size attribute"
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        " Setter method for size attribute"
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    @property
+    def position(self):
+        " Getter method for position attribute"
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        " Setter method for position attribute"
+        if not isinstance(value, tuple) or len(value) != 2 or \
+                not all(isinstance(val, int) for val in value) or \
+                any(val < 0 for val in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        " Calculates area of square"
+        return self.__size ** 2
+
+    def my_print(self):
+        " Prints in stdout the square with #"
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__position[1]):
+                print("")
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
