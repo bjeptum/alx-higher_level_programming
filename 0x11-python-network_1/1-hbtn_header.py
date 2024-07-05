26734ce20045e07d40c3b9423338024191763dc2
bjeptum <brendajeptum@gmail.com>
2024-07-05 20:18:07 +0000

    POST an email #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,12 +4,11 @@ Takes in URL, sends a request
 Displays the value of the X-Request-Id variable
 found in the header of the response
 """
-
 import sys
 import urrlib.request
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     url = sys.argv[1]
     with urllib.request.urlopen(url) as resp:
