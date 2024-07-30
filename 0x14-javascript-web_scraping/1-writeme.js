42b62104b3af886c0c18947e8a9f55f1e7cb5c4e
b_jeptum <brendajeptum@gmail.com>
2024-07-31 00:41:23 +0300

    0x14. JavaScript - Web scraping tasks

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Writes a string to a file
+ */
+const fs = require('fs');
+const file = process.argv[2];
+const newString = process.argv[3];
+
+fs.writeFile(file, newString, 'utf-8', function (err, data) {
+  if (err) {
+    console.error(err);
+  }
+});
