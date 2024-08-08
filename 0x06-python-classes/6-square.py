36ff08a77c880135265bed490023c20bad581126
b_jeptum <brendajeptum@gmail.com>
2024-08-08 09:09:33 +0300

    Updated instantiation of class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,8 +11,8 @@ class Square:
     """
     def __init__(self, size=0, position=(0, 0)):
         " Class instantiator"
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
